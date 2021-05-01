2b31029c07b2e616bdd0a93d45e5f8ce2717c608
Artem Bykov <bykovaa94@mail.ru>
2021-05-01 19:58:51 +0300

    editing end add new function in brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -2,6 +2,7 @@
 import readlineSync from 'readline-sync';
 import mathemOperations from '../src/mathemOperations.js';
 import randomNum from '../src/randomNum.js';
+import randomSign from '../src/randomSign.js';
 
 const name = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
 
@@ -10,13 +11,11 @@ console.log('What is the result of the expression?');
 
 let i = 1;
 while (i < 4) {
-  const computationSign = randomNum(0, 2);
-  const sign = ['+', '-', '*'];
-  const randomComputationSign = sign[computationSign];
   const ranNum1 = randomNum(0, 10);
   const ranNum2 = randomNum(0, 10);
-  console.log(`Question: ${ranNum1}${randomComputationSign}${ranNum2}`);
-  const sumRanNum = mathemOperations(randomComputationSign, ranNum1, ranNum2);
+  const ranSig = randomSign();
+  const sumRanNum = mathemOperations(ranSig, ranNum1, ranNum2);
+  console.log(`Question: ${ranNum1}${ranSig}${ranNum2}`);
   const answerQues = readlineSync.question('Your answer: ');
   if (Number(answerQues) !== sumRanNum) {
     console.log(`"${answerQues}" is wrong answer ;(. Correct answer was "${sumRanNum}"\nLet's try again, ${name}!`);
