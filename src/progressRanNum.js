6d689f7c0e995b77401f65d1b12998d4732f6e22
Artem Bykov <bykovaa94@mail.ru>
2021-05-01 20:09:26 +0300

    correct function progressRanNum.js

--- src/progressRanNum.js
+++ src/progressRanNum.js
@@ -3,7 +3,8 @@ import randomNum from './randomNum.js';
 const progressRanNum = (arr) => {
   const randNumCount = randomNum(0, arr.length - 1);
   const dotter = '..';
-  arr[randNumCount] = dotter;
-  return arr.join(' ');
+  const newArr = arr;
+  newArr[randNumCount] = dotter;
+  return newArr.join(' ');
 };
 export default progressRanNum;
