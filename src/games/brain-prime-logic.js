cda7d560b61fba2a99053e4d634dfdbeb4b6379a
Artem Bykov <bykovaa94@mail.ru>
2021-06-01 22:15:48 +0300

    fix brain-prime-logic

--- src/games/brain-prime-logic.js
+++ src/games/brain-prime-logic.js
@@ -2,15 +2,17 @@ import startGame from '../index.js';
 import helpers from '../helpers.js';
 
 const checksSimpleNum = (number) => {
+  let result;
   for (let i = 2; i < number; i += 1) {
     if (number % i === 0) {
-      return;
+      result = 0;
     }
   }
+  return result;
 };
 const generateRound = () => {
   const num = helpers.generateRandomNumber(1, 100);
-  const isSimpleNum = (checksSimpleNum(num) ? 'no' : 'yes');
+  const isSimpleNum = (checksSimpleNum(num) === 0 ? 'no' : 'yes');
   const qustion = String(num);
   const answer = String(isSimpleNum);
   return [qustion, answer];
