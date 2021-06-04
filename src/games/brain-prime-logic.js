50d09dfa56841f350341f4d054e603f73b401e1a
Artem Bykov <bykovaa94@mail.ru>
2021-06-04 17:22:56 +0300

    fit brain-prime-logic

--- src/games/brain-prime-logic.js
+++ src/games/brain-prime-logic.js
@@ -4,15 +4,19 @@ import helpers from '../helpers.js';
 const isPrime = (number) => {
   for (let i = 2; i < number; i += 1) {
     if (number % i === 0) {
-      return true;
+      return false;
     }
   }
-  return false;
+  return true;
 };
+
 const generateRound = () => {
   const num = helpers.generateRandomNumber(1, 100);
-  const isSimpleNum = (isPrime(num) === true ? 'no' : 'yes');
-  const qustion = (helpers.generateRandomNumber(1, 100)).toString();
+  const isSimpleNum = (isPrime(num) === true ? 'yes' : 'no');
+  console.log(isPrime(num));
+  console.log(isSimpleNum);
+  console.log(num);
+  const qustion = num.toString();
   const answer = isSimpleNum;
   return [qustion, answer];
 };
