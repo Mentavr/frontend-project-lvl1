7a9378ecd286aa0fb4f0e1311fbda2bfd64e1df3
Artem Bykov <bykovaa94@mail.ru>
2021-06-04 16:59:46 +0300

    fix brain-progression-logic

--- src/games/brain-progression-logic.js
+++ src/games/brain-progression-logic.js
@@ -4,7 +4,7 @@ import helpers from '../helpers.js';
 const getProgression = (first, step, length) => {
   const arrNumProgress = [];
   let lengthProgression = length;
-  for (let i = first; lengthProgression > 0; i += step, lengthProgression -= 1) {
+  for (let i = first; lengthProgression >= 0; i += step, lengthProgression -= 1) {
     arrNumProgress.push(i);
   }
   return arrNumProgress;
@@ -15,8 +15,7 @@ const generateRound = () => {
   const firstNum = helpers.generateRandomNumber(1, 100);
   const lengthProgress = helpers.generateRandomNumber(6, 10);
   const arrGetProgression = getProgression(firstNum, stepProgress, lengthProgress);
-  const firstIndex = 0;
-  const indexProgress = helpers.generateRandomNumber(firstIndex, lengthProgress);
+  const indexProgress = helpers.generateRandomNumber(0, lengthProgress);
   const answer = arrGetProgression[indexProgress].toString();
   arrGetProgression[indexProgress] = '..';
   const qustion = arrGetProgression.join(' ');
